9106d1982232ade79ce9eac88d9f16aadf37b0c4
Yassin HAMMOU OUALI <y.hammououali@gmail.com>
2019-02-10 19:50:21 +0100

    Adding annotation packages & use it on project's

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('OMG! My first Symfony page! :D');
