b890f4fe0dae2a936de47a4632cb91867be58ce6
Yassin HAMMOU OUALI <y.hammououali@gmail.com>
2019-02-10 20:00:38 +0100

    Adding wildcard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,4 +14,27 @@ class HomeController
     {
         return new Response('OMG! My first Symfony page! :D');
     }
+
+    /**
+     * @Route("/article/why-you-should-learn-symfony")
+     */
+//    public function read()
+//    {
+//        return new Response('Article is coming very soon!');
+//    }
+
+    /**
+     * Using wildcard
+     *
+     * Doc: https://symfony.com/doc/current/routing.html#adding-wildcard-requirements
+     *
+     * @Route("/article/{slug}")
+     */
+    public function read($slug)
+    {
+        return new Response(sprintf(
+            'Article: "%s" is coming very soon',
+            $slug
+        ));
+    }
 }
