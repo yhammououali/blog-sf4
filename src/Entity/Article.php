56986d673cd981b9bb8ebe7378ace3b84856d7a8
Yassin HAMMOU OUALI <y.hammououali@gmail.com>
2019-02-18 00:51:59 +0100

    Adding validator component & add a validation rule on entity

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
@@ -33,6 +34,7 @@ class Article
 
     /**
      * @var @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
      */
     private $author;
 
